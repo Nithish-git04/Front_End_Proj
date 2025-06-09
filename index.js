5a4f90ebdbfb48e36f3a39ee7687f8f378d0a303
Nithish0405 <nithishthanigaivel.2005@gmail.com>
2025-06-09 21:31:36 +0530

    minor changes in catalogue

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ closeBtn.addEventListener('click', () => {
 const getintouch = document.getElementById('get_in_touch_1'); // Get the button.
 
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 1050) { 
+    if (window.pageYOffset > 1150) { 
         getintouch.classList.add('show');
     } else {
         getintouch.classList.remove('show');
