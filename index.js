d487b562a5d76692141436e0c5b0d4a5d7c0e40e
Nithish0405 <nithishthanigaivel.2005@gmail.com>
2025-06-09 21:13:51 +0530

    Created the catalogue page

--- index.js
+++ index.js
@@ -16,9 +16,42 @@ closeBtn.addEventListener('click', () => {
 const getintouch = document.getElementById('get_in_touch_1'); // Get the button.
 
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 10) { //will increase offset once the other sections are added
+    if (window.pageYOffset > 970) { 
         getintouch.classList.add('show');
     } else {
         getintouch.classList.remove('show');
     }
 });
+
+//making the catalogue image rollover
+let currentPage = 0;
+
+function moveSlider(direction) {
+  const track = document.getElementById('imageTrack');
+  const pages = track.querySelectorAll('.image-page');
+  const totalPages = pages.length;
+
+  console.log(totalPages);
+
+  currentPage += direction;
+
+  if (currentPage < 0) currentPage = 0;
+  if (currentPage >= totalPages) currentPage = totalPages - 1;
+
+  track.style.transform = `translateX(-${currentPage * 900}px)`;
+
+  const leftBtn = document.getElementById('left_btn');
+  const rightBtn = document.getElementById('right_btn');
+
+  if (currentPage === 0) {
+    leftBtn.classList.add('disabled');
+  } else {
+    leftBtn.classList.remove('disabled');
+  }
+
+  if (currentPage === totalPages - 1) {
+    rightBtn.classList.add('disabled');
+  } else {
+    rightBtn.classList.remove('disabled');
+  }
+}
