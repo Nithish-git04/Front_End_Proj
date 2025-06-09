ed9ae46bec4c30e26c6c6ca9de5beb08fcd7e195
varkan15 <var.kandukuri@gmail.com>
2025-06-09 22:02:16 +0530

    minor changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ closeBtn.addEventListener('click', () => {
 const getintouch = document.getElementById('get_in_touch_1'); // Get the button.
 
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 1150) { 
+    if (window.pageYOffset > 900) { 
         getintouch.classList.add('show');
     } else {
         getintouch.classList.remove('show');
